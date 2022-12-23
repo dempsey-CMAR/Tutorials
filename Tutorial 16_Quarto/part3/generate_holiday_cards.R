9f3b17b60ea37e2be5f63cca701b3e3a758c8515
Danielle Dempsey <ddempsey@perennia.ca>
2022-12-23 08:47:45 -0400

    presented tutorial

---
+++ Tutorial 16_Quarto/part3/generate_holiday_cards.R
@@ -0,0 +1,22 @@
+library(here)
+library(quarto)
+
+card_template <- paste0("Tutorial 16_Quarto/part3/holiday_card_generic.qmd")
+
+to <- c("Anne", "Jenny", "Kiersten", "Nicole", "Raeleigh", "Ryan")
+
+# Because of the way the Tutorials project is set up, the cards will be 
+# exported to the Tutorials folder
+sapply(to, function(x) { 
+  
+  quarto_render(
+    input = card_template, 
+    output_file = paste0(x, "_card.pdf"),
+    execute_params = list(to = x, from = "Danielle"))
+})
+
+
+
+
+
+
